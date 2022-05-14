86f326f45b13eeee447e54d1af2dc23f0d029f88
Unauthorized23 <105158734+Unauthorized23@users.noreply.github.com>
2022-05-14 11:00:31 +0630

    Add project files.

---
+++ Nebula Authentication App/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace LearningC
+{
+    public class Learning
+    {
+        static void Main(string[] args)
+        {
+
+            // Verified Details Table
+            string verifiedNames = "Guest";
+            string verifiedPassword = "Guest";
+            string verifiedWebsites = "Roblox";
+
+            // Asking For Inputs
+            Console.Title = "Nebula TokenAuthenticator for ROBLOX";
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            Console.WriteLine("What website are you searching?");
+
+            string webSite = Console.ReadLine();
+
+            Console.WriteLine("Enter your username:");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Enter your password:");
+
+            string passWord = Console.ReadLine();
+
+            // Making Inputs Function
+            if (userName == verifiedNames)
+            {
+                Console.WriteLine("Welcome " + userName);
+
+            }
+            else
+            {
+                Console.WriteLine("There is no such account with the name: " + userName);
+            }
+
+            if (passWord == verifiedPassword)
+            {
+
+                Console.WriteLine("Unable to fecth Account token");
+
+            }
+            else
+            {
+                Console.WriteLine("Your password is incorrect");
+            }
+
+            if (webSite == verifiedWebsites)
+            {
+                Console.WriteLine("The account is possible at Roblox.com");
+            }
+            else
+            {
+                Console.WriteLine("There is no such account in the website: " + webSite);
+            }
+
+        }
+
+
+
+    }
+}
